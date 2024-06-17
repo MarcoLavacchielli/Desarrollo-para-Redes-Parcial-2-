cb877d5abaea3cf7e521ca391316e22a20857fdb
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2024-06-17 18:59:02 -0300

    algo que no dejaba exportar

--- Assets/Scripts/Host/Main Menu/MainMenuHandler.cs
+++ Assets/Scripts/Host/Main Menu/MainMenuHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using TMPro.EditorUtilities;
+//using TMPro.EditorUtilities;
 
 public class MainMenuHandler : MonoBehaviour
 {
